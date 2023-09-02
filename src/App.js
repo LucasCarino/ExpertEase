02d792bdcc25b12181149d493c1db8214aebf849
mafebohorquezh <mafebohorquezh@gmail.com>
2023-09-02 16:11:17 -0300

    Add footer to all pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from "./components/Home";
 import Header from "./components/Header";
 import About from "./components/About";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/" element={<Home/>}/>
         <Route exact path="/About" element={<About/>}/>
       </Routes>
+      <Footer />
     </Router>
   );
 }
